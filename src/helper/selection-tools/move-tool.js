831f27bddb24530cd59d0b1b70ed2a8b9e64ab14
DD Liu <liudi@media.mit.edu>
2019-10-16 14:43:05 -0400

    lint

--- src/helper/selection-tools/move-tool.js
+++ src/helper/selection-tools/move-tool.js
@@ -98,7 +98,6 @@ class MoveTool {
         this.setSelectedItems();
     }
     onMouseDrag (event) {
-        const size = paper.DomElement.getSize(paper.view.element);
         const point = event.point;
         const bounds = getActionBounds();
         
